c07322c5e28f0f942b5a5519674a81cc824056a1
lcuiti30-ctrl <lcuiti30@gmail.com>
2025-11-04 19:27:18 -0300

    cambios

--- hackflix/src/App.jsx
+++ hackflix/src/App.jsx
@@ -1,11 +1,29 @@
 import { useState } from "react";
 import "./App.css";
-import Barra from "./navbar";
+import Barra from "./componentes/Barra";
+import { Rating } from "react-simple-star-rating";
 
 function App() {
+  const [rating, setRating] = useState(0);
+
+  // Catch Rating value
+  const handleRating = (rate) => {
+    setRating(rate);
+  };
+
+  const handleReset = () => {
+    // Set the initial value
+    setRating(0);
+  };
   return (
     <>
       <Barra />
+      <div className="contenedor rating">
+        <h4>Calificá este producto:</h4>
+        <Rating onClick={handleRating} initialValue={rating} />
+
+        <button onClick={handleReset}>reset</button>
+      </div>
     </>
   );
 }
