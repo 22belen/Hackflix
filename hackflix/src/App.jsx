fad4b36848315d80f180f08e2b62b57a6ccf21d5
22belen <belen.fleitas@anima.edu.uy>
2025-11-04 20:07:01 -0300

    cards

--- hackflix/src/App.jsx
+++ hackflix/src/App.jsx
@@ -2,17 +2,17 @@ import { useState } from "react";
 import "./App.css";
 import Barra from "./componentes/Barra";
 import { Rating } from "react-simple-star-rating";
+import movies from "./db/movies.json";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
   const [rating, setRating] = useState(0);
 
-  // Catch Rating value
   const handleRating = (rate) => {
     setRating(rate);
   };
 
   const handleReset = () => {
-    // Set the initial value
     setRating(0);
   };
   return (
@@ -21,9 +21,24 @@ function App() {
       <div className="contenedor rating">
         <h4>Calificá este producto:</h4>
         <Rating onClick={handleRating} initialValue={rating} />
-
         <button onClick={handleReset}>reset</button>
       </div>
+      <Container>
+        <Row>
+          {movies.map((movie) => (
+            <Col md={4} key={movie.id}>
+              <div className="movie-card">
+                <img
+                  src={movie.poster_path}
+                  alt={movie.title}
+                  className="poster"
+                />
+                <h5 className="nombre">{movie.title}</h5>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </>
   );
 }
