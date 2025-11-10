3df7884737fd985a7553221ca8f1ef109bc1cc4c
22belen <belen.fleitas@anima.edu.uy>
2025-11-10 20:32:19 -0300

    scroll

--- hackflix/src/App.jsx
+++ hackflix/src/App.jsx
@@ -1,62 +1,14 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
-import Barra from "./componentes/Barra";
+import Barra from "./components/Barra";
 import { Rating } from "react-simple-star-rating";
-import { Container, Row, Col } from "react-bootstrap";
-import axios from "axios";
+import Scroll from "./components/Scroll";
 
 function App() {
-  const [rating, setRating] = useState(0);
-  const [movies, setMovies] = useState([]);
-  const [page, setPage] = useState(1);
-
-  useEffect(() => {
-    const getMovies = async () => {
-      const response = await axios.get(
-        `https://api.themoviedb.org/3/movie/popular?api_key=2cb2d43409108d6f09870f3c3c531b0c&page=${page}`
-      );
-      setMovies(response.data.results);
-      console.log(response);
-    };
-    getMovies();
-  }, []);
-
-  function handleRating(rate) {
-    const realRating = rate * 2;
-    setRating(realRating);
-  }
-
-  function handleReset() {
-    setRating(0);
-  }
-
-  const filteredMovies = movies.filter((movie) => movie.vote_average >= rating);
-
   return (
     <>
       <Barra />
-      <div className="contenedor rating">
-        <h4>Calificá este producto:</h4>
-        <Rating onClick={handleRating} initialValue={(rating / 10) * 5} />
-        <button onClick={handleReset}>reset</button>
-        <p>Puntaje seleccionado: {rating}</p>
-      </div>
-      <Container>
-        <Row>
-          {filteredMovies.map((movie) => (
-            <Col md={4} key={movie.id}>
-              <div className="movie-card">
-                <img
-                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                  alt={movie.title}
-                  className="poster"
-                />
-                <h5 className="nombre">{movie.title}</h5>
-              </div>
-            </Col>
-          ))}
-        </Row>
-      </Container>
+      <Scroll />
     </>
   );
 }
---
+++ hackflix/src/components/Scroll.jsx
@@ -0,0 +1,87 @@
+import { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import axios from "axios";
+import { Container, Row, Col } from "react-bootstrap";
+import { Rating } from "react-simple-star-rating";
+
+function Scroll() {
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const [rating, setRating] = useState(0);
+
+  function handleRating(rate) {
+    const realRating = rate * 2;
+    setRating(realRating);
+  }
+
+  function handleReset() {
+    setRating(0);
+  }
+
+  const getMovies = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=2cb2d43409108d6f09870f3c3c531b0c&page=${page}`
+      );
+      const newMovies = response.data.results;
+
+      setMovies((prevMovies) => {
+        const existingIds = new Set(prevMovies.map((m) => m.id));
+        const uniqueMovies = newMovies.filter((m) => !existingIds.has(m.id));
+        return [...prevMovies, ...uniqueMovies];
+      });
+
+      if (newMovies.length === 0) {
+        setHasMore(false);
+      }
+
+      setPage((prevPage) => prevPage + 1);
+    } catch (error) {
+      console.log("Ocurrió un error al cargar las películas", error);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  const filteredMovies = movies.filter((movie) => movie.vote_average >= rating);
+
+  return (
+    <div>
+      <div className="contenedor rating">
+        <h4>Calificá este producto:</h4>
+        <Rating onClick={handleRating} initialValue={(rating / 10) * 5} />
+        <button onClick={handleReset}>reset</button>
+        <p>Puntaje seleccionado: {rating}</p>
+      </div>
+      <InfiniteScroll
+        dataLength={filteredMovies.length}
+        next={getMovies}
+        hasMore={hasMore}
+        loader={<h4>Cargando más películas...</h4>}
+        endMessage={<p>No hay más películas para mostrar</p>}
+      >
+        <Container>
+          <Row>
+            {filteredMovies.map((movie) => (
+              <Col md={4} key={movie.id}>
+                <div className="movie-card">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                    alt={movie.title}
+                    className="poster"
+                  />
+                  <h5 className="nombre">{movie.title}</h5>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </InfiniteScroll>
+    </div>
+  );
+}
+
+export default Scroll;
